76e2ae6132462754bfff94072c43dc3bc5698ebb
M-Ahmd <ma09500@gmail.com>
2025-07-08 15:28:55 +0300

    create book class which is a general class

---
+++ src/Book.java
@@ -0,0 +1,20 @@
+public abstract class Book {
+    String ISBN;
+    String title;
+    int publicationYear;
+
+    public Book(String ISBN, String title, int publicationYear) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+    public String getISBN() {
+        return ISBN;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+}
