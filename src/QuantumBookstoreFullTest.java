63f0134e96ef343ae6d1a65a1de73af8e86f13eb
M-Ahmd <ma09500@gmail.com>
2025-07-08 16:14:07 +0300

    added some functions to Library class and test all classes

--- src/QuantumBookstoreFullTest.java
+++ src/QuantumBookstoreFullTest.java
@@ -2,7 +2,7 @@ public class QuantumBookstoreFullTest {
     public static void main(String[] args) throws Exception {
         Book book = new PaperBook("1234567890", "Quantum Physics", 2023, 29.99, 100);
         Book ebook = new Ebook("0987654321", "Quantum Mechanics", 2022, 19.99);
-        Book demoBook = new DemoBook("1122334455", "Quantum Computing", 2019);
+        Book demoBook = new DemoBook("1122334455", "Quantum Computing", 2023);
         Library library = new Library();
         library.addBook(book);
         library.addBook(ebook);
